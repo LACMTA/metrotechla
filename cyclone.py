68cdc279e8ba980fd4b21db8ad59b7bf4a192a87
Douglas Goodwin <dgoodwin@gmail.com>
2014-11-06 12:05:59 -0800

    add instructions

--- cyclone.py
+++ cyclone.py
@@ -17,3 +17,18 @@ application = Application([
 if __name__ == "__main__":
 	application.listen(8000)
 	IOLoop.instance().start()
+
+"""
+; supervisor script
+; This is where you run individual Tornado instances.
+; We run four; one per processor core.
+; In development, we ran as many as four per core with no issues.
+; If you're looking to minimize cpu load, run fewer processes.
+; BTW, Tornado processes are single threaded.
+; To take advantage of multiple cores, you'll need multiple processes.
+ 
+[program:tornado-8000]
+command=/var/www/envs/metrotechla/bin/python /var/www/envs/metrotechla/cyclone.py
+stderr_logfile = /var/log/supervisord/tornado_metrotechla-stderr.log
+stdout_logfile = /var/log/supervisord/tornado_metrotechla-stdout.log
+"""
